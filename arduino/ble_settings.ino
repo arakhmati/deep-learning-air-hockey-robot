c48435100e74fdf97fedd276f6b3211afb66ba16
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2017-11-28 01:42:24 -0500

    Added arduino program for configuring HM-10 settings

---
+++ arduino/ble_settings.ino
@@ -0,0 +1,57 @@
+//  SerialIn_SerialOut_HM-10_01
+//
+//  Uses hardware serial to talk to the host computer and AltSoftSerial for communication with the bluetooth module
+//
+//  What ever is entered in the serial monitor is sent to the connected device
+//  Anything received from the connected device is copied to the serial monitor
+//  Does not send line endings to the HM-10
+//
+//  Pins
+//  BT VCC to Arduino 5V out. 
+//  BT GND to GND
+//  Arduino D8 (SS RX) - BT TX no need voltage divider 
+//  Arduino D9 (SS TX) - BT RX through a voltage divider (5v to 3.3v)
+//
+char c=' ';
+boolean NL = true;
+char buffer[100];
+ 
+void setup() 
+{
+    Serial.begin(115200);
+    Serial.print("Sketch:   ");   Serial.println(__FILE__);
+    Serial.print("Uploaded: ");   Serial.println(__DATE__);
+    Serial.println(" ");
+
+    long bt_baud = 115200;
+    sprintf (buffer, "BTserial started at %ld", bt_baud);
+    
+    Serial1.begin(bt_baud);  
+    Serial.println(buffer);
+}
+ 
+void loop()
+{
+    // Read from the Bluetooth module and send to the Arduino Serial Monitor
+    if (Serial1.available())
+    {
+        c = Serial1.read();
+        Serial.write(c);
+    }
+ 
+    // Read from the Serial Monitor and send to the Bluetooth module
+    if (Serial.available())
+    {
+        c = Serial.read();
+ 
+        // do not send line end characters to the HM-10
+        if (c!=10 & c!=13 ) 
+        {  
+             Serial1.write(c);
+        }
+ 
+        if (NL) { Serial.print("\r\n>");  NL = false; }
+        Serial.write(c);
+        if (c==10) { NL = true; }
+    }
+}
