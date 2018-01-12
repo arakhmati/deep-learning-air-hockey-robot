e8e5c4f6d0728c06601236a59aceec70e9047f6a
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2018-01-11 23:27:05 -0500

    Skip frames randomly in generate_data and in train_dqn. In train_dqn, changed softmax activation to linear activation
    Former-commit-id: 5b2a6f4bffde9fb65caf132e66823e4db2995824

--- ai/reinforcement/plot_utils.py
+++ ai/reinforcement/plot_utils.py
@@ -1,26 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot2D(array):
-    plt.figure(0)
-    plt.imshow(np.repeat(array, repeats=5, axis=0))
+def plot_state(state):
+    state = np.uint8(np.copy(state) * 128 + 128)
+    f, ax = plt.subplots(1, 3)
+    ax[0].imshow(state[0:3].transpose((1,2,0)))
+    ax[1].imshow(state[3:6].transpose((1,2,0)))
+    ax[2].imshow(state[6:9].transpose((1,2,0)))
     plt.show()
-
-def plotState(array, n_cols=3):
-    plt.figure(0)
-    n, h, w = array.shape
-    n_cols = min(n, n_cols)
-    n_rows = int(np.ceil(n/n_cols))
-    for i in range(n_rows):
-        for j in range(n_cols):
-            if i*n_cols+j < n:
-                plt.subplot2grid((n_rows, n_cols), (i, j))
-                plt.imshow(array[i*n_cols+j].reshape((h, w)))
-    plt.show()
-
-def plot(array, n_cols=3):
-
-    if len(array.shape) == 2:
-        plot2D(array)
-    elif len(array.shape) ==3:
-        plot3D(array, n_cols)
