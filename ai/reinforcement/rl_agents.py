a2b3fc1009a253e75544e8f57679083cb4f86fcf
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2018-01-31 23:27:28 -0500

    Added Policy Gradient. Tested it on cartpole. Refactored RL directory
    Former-commit-id: cc9d7ad07f0efdb0f91cec9b8a4f90f73244549c

---
+++ ai/reinforcement/rl_agents.py
@@ -0,0 +1,271 @@
+import os
+import sys
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dir_path + '/../')
+sys.path.append('../supervised/keras')
+
+import numpy as np
+from abc import ABC, abstractmethod
+from collections import deque
+
+import matplotlib.pyplot as plt
+from keras.layers import Dense
+from keras.models import  clone_model, load_model, Model
+from keras.optimizers import SGD
+
+from metrics import fmeasure, recall, precision
+
+class Agent(ABC):
+    
+    def __init__(self,
+                 n_actions,
+                 buffer_size=1000,
+                 batch_size=128,
+                 discount_rate=0.99,
+                 policy=None,
+                 eps_min=0.0,
+                 eps_max=0.9,
+                 eps_decay_steps=1000,
+                 model_file='rl_model.h5'):
+
+        self.iteration = 0
+        self.use_target = False
+        
+        self.n_actions = n_actions
+        self.batch_size = batch_size
+        self.buffer_size = buffer_size
+        self.discount_rate = discount_rate
+        
+        self._reset_experience_buffer()
+        self.loss_buffer = deque([], maxlen=100000)
+        
+        self.model_file = model_file
+        
+        if policy is None:
+            self.eps_min = eps_min
+            self.eps_max = eps_max
+            self.eps_decay_steps = eps_decay_steps
+            def epsilon_greedy(q_values):
+                epsilon = max(eps_min, eps_max - (eps_max - eps_min) * self.iteration / eps_decay_steps)
+                if self.iteration % 100 == 0:
+                    print('Epsilon %f' % epsilon)
+                if np.random.uniform(0, 1) < epsilon:
+                    return np.random.randint(self.n_actions)
+                return np.argmax(q_values)
+            self.policy = epsilon_greedy
+            
+    def _reset_experience_buffer(self):
+        self.experience_buffer = deque([], maxlen=self.buffer_size)
+            
+    def _sample_experiences(self, batch_size):
+        
+#        for state, action, reward, next_state, done in self.experience_buffer:
+#            from plot_utils import plot_states
+#            plot_states(state, action, reward, next_state, done)
+        
+        from random import sample
+        min_len = min(batch_size, len(self.experience_buffer))
+        random_memories = sample(self.experience_buffer, min_len)
+        
+        transposed = [[], [], [], [], []]
+        for memory in random_memories:
+            for transposed_row, value in zip(transposed, memory):
+                transposed_row.append(value)
+        return [np.array(transposed_row) for transposed_row in transposed]
+
+    def _predict(self, X, use_target=False, expand_dims=False):
+        if expand_dims:
+            X = np.expand_dims(X, axis=0)
+        if use_target:
+            return self.target_model.predict(X)
+        return self.model.predict(X)
+        
+    def store_experience(self, memory):
+        state, action, reward, next_state, done = memory
+        self.experience_buffer.append((np.copy(state), action, reward, np.copy(next_state), done))
+        
+    def act(self, state):
+        self.iteration += 1
+        prediction = self._predict(state, expand_dims=True)
+        action = self.policy(prediction)
+        self.prediction = prediction[0]
+        return action
+        
+    def update_target_weights(self):
+        self.target_model.set_weights(self.model.get_weights())
+        self.use_target = True
+        self.model.save(self.model_file)
+        print('Copied weights from model to target_model')
+
+    def _set_target_model(self):
+        self.target_model = clone_model(self.model)
+        self.target_model.compile(
+                               loss='mean_squared_error',  
+                               optimizer=SGD(lr=0.0075, momentum=0.5, decay=1e-6, clipnorm=2),
+                               metrics=['accuracy'])
+        self.target_model.set_weights(self.model.get_weights())
+        self.target_model.summary()
+   
+        for model_weight, target_model_weight in zip(self.model.get_weights(), self.target_model.get_weights()):
+            equals = np.allclose(model_weight, target_model_weight)
+            assert equals
+
+class DDQNAgent(Agent):
+    
+    def __init__(self,
+                 n_actions,
+                 initial_model_file=None,
+                 model=None,
+                 buffer_size=1000,
+                 batch_size=128,
+                 discount_rate=0.99,
+                 policy=None,
+                 eps_min=0.0,
+                 eps_max=0.9,
+                 eps_decay_steps=1000,
+                 model_file='ddqn_model.h5',
+                 replace_softmax=True):
+                 
+        super().__init__(
+            n_actions=n_actions,
+            buffer_size=buffer_size,
+            batch_size=batch_size,
+            discount_rate=discount_rate,
+            policy=policy,
+            eps_min=eps_min,
+            eps_max=eps_max,
+            eps_decay_steps=eps_decay_steps,
+            model_file=model_file
+        )
+
+        if initial_model_file != None:
+            self.model = load_model(initial_model_file, {'fmeasure': fmeasure, 'recall': recall, 'precision': precision})
+        elif model != None:
+            self.model = model
+        else:
+            raise ValueError('A file containing model or a model object needs to specified')
+
+        if replace_softmax:
+            # Replace softmax layer with linear activation
+            # By giorgiop on https://github.com/keras-team/keras/issues/3465 commented on 10 Oct 2016
+            self.model.layers.pop()
+            self.model.layers[-1].outbound_nodes = []
+            self.model.outputs = [self.model.layers[-1].output]
+            output = self.model.layers[-1].output
+            output = Dense(units=self.n_actions, name='output', activation='linear')(output) # your newlayer Dense(...)
+            self.model = Model(self.model.input, output)
+            self.model.compile(
+                          loss='mean_squared_error',  
+                          optimizer=SGD(lr=0.0075, momentum=0.5, decay=1e-6, clipnorm=2),
+                          metrics=['accuracy'])
+        
+        self.model.summary()
+        self._set_target_model()
+        
+    def train(self, plot=True):
+        states, actions, rewards, next_states, done = self._sample_experiences(self.batch_size)
+        
+        targets = self._predict(states)
+        next_q_values = self._predict(next_states, use_target=self.use_target)
+        next_actions = np.argmax(self._predict(next_states), axis=1)
+        
+        row_indices = np.arange(targets.shape[0])
+        targets[row_indices, actions] = rewards + (1 - done) * self.discount_rate * next_q_values[row_indices, next_actions]
+        
+        # # Prevent the logits from growing too big
+        # targets = np.clip(targets, -1, 1)
+
+        loss = self.model.fit(states, targets, epochs=1, verbose=0, shuffle=True)
+        self.loss_buffer.append(loss.history['loss'])
+
+        if plot:
+            plt.clf()
+            plt.plot(self.loss_buffer)
+            plt.pause(0.1)
+            
+class PGAgent(Agent):
+    
+    def __init__(self,
+                 n_actions,
+                 initial_model_file=None,
+                 model=None,
+                 buffer_size=1000,
+                 batch_size=128,
+                 discount_rate=0.99,
+                 policy=None,
+                 eps_min=0.0,
+                 eps_max=0.9,
+                 eps_decay_steps=1000,
+                 model_file='pg_model.h5',
+                 learning_rate=0.01):
+                 
+                 
+        super().__init__(
+            n_actions=n_actions,
+            buffer_size=buffer_size,
+            batch_size=batch_size,
+            discount_rate=discount_rate,
+            policy=policy,
+            eps_min=eps_min,
+            eps_max=eps_max,
+            eps_decay_steps=eps_decay_steps,
+            model_file=model_file
+        )
+        self.learning_rate = learning_rate
+
+        if initial_model_file != None:
+            self.model = load_model(initial_model_file, {'fmeasure': fmeasure, 'recall': recall, 'precision': precision})
+        elif model != None:
+            self.model = model
+        else:
+            raise ValueError('A file containing model or a model object needs to specified')
+
+    def _sample_experiences(self):
+
+        memories = list(self.experience_buffer)
+
+        transposed = [[], [], [], []]
+        for memory in memories:
+            for transposed_row, value in zip(transposed, memory):
+                transposed_row.append(value)
+        return [np.array(transposed_row) for transposed_row in transposed]
+        
+    def store_experience(self, memory):
+        state, action, reward, _, _ = memory
+        
+        y = np.zeros_like(self.prediction, dtype=np.float32)
+        y[action] = 1.0
+
+        # self.prediction is set in act() which should always be called before this method
+        self.experience_buffer.append((np.copy(state), reward, self.prediction, y - self.prediction))
+        
+    def train(self, plot=True):
+        def discount_rewards(rewards):
+            rewards = np.array(rewards)
+            # Discount episode rewards
+            for i in reversed(range(1, len(rewards))):
+                rewards[i-1] += self.discount_rate * rewards[i]
+            # standardize the rewards to be unit normal (helps control the gradient estimator variance)
+            rewards -= np.mean(rewards)
+
+            # Fix division by zero bug
+            rewards_std = np.std(rewards)
+            rewards = np.divide(rewards, rewards_std, out=np.zeros_like(rewards), where=rewards_std!=0)
+
+            return rewards
+            
+        states, rewards, predictions, gradients = self._sample_experiences()
+        rewards = discount_rewards(rewards)
+        gradients *= rewards.reshape((rewards.shape[0], 1))
+        
+        targets = predictions + self.learning_rate * gradients
+        
+        loss = self.model.fit(states, targets, epochs=1, verbose=0, shuffle=True)
+        self.loss_buffer.append(loss.history['loss'])
+        
+        self._reset_experience_buffer()
+
+        if plot:
+            plt.clf()
+            plt.plot(self.loss_buffer)
+            plt.pause(0.00001)
--- ai/reinforcement/train.py
+++ ai/reinforcement/train.py
@@ -1,19 +1,19 @@
+import os
 import sys
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dir_path + '/../')
 sys.path.append('../supervised/keras')
+
+import config
         
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import deque
 
-from keras.layers import Dense
-from keras.models import  clone_model, load_model, Model
-from keras.optimizers import SGD
-
 import gym
 import gym_air_hockey
 
-from ddqn import DDQNAgent
-from metrics import fmeasure, recall, precision
+from rl_agents import DDQNAgent, PGAgent
 from plot_utils import plot_states
 
 models_dir = dir_path + '/models/' + config.mode + '/'
@@ -25,7 +25,7 @@ human_model_file = models_dir + human_model_name + '.h5'
 
 if __name__ == "__main__":
 
-    n_episodes = 20000
+    n_episodes = 200000
     episode_length = 200
     training_start = 100
     training_interval = 25
@@ -34,82 +34,34 @@ if __name__ == "__main__":
     batch_size = 64
     discount_rate = 0.99
     buffer_size = 10000
-    eps_min = 0.0
+    eps_min = 0.1
     eps_max = 0.99
-    eps_decay_steps = 100000
-    
-    reward_buffer = deque([], maxlen=100)
-    
-    def models():
-        model = load_model('../supervised/keras/models/rgb/robot_model.h5', {'fmeasure': fmeasure, 'recall': recall, 'precision': precision})
-
-        # Replace softmax layer with linear activation
-        # By giorgiop on https://github.com/keras-team/keras/issues/3465 commented on 10 Oct 2016
-        model.layers.pop()
-        model.layers[-1].outbound_nodes = []
-        model.outputs = [model.layers[-1].output]
-        output = model.get_layer('dense1').output
-        output = Dense(units=10, name='output', activation='linear')(output) # your newlayer Dense(...)
-        model = Model(model.input, output)
-        model.compile(loss='mean_squared_error',  optimizer=SGD(lr=0.0075, momentum=0.5, decay=1e-6, clipnorm=2),
-                      metrics=['accuracy'])
-    
-        target_model = clone_model(model)
-        target_model.compile(loss='mean_squared_error',  optimizer=SGD(lr=0.0075, momentum=0.5, decay=1e-6, clipnorm=2),
-                      metrics=['accuracy'])
-        target_model.set_weights(model.get_weights())
-        
-        model.summary()
-        target_model.summary()
-    
-        for model_weight, target_model_weight in zip(model.get_weights(), target_model.get_weights()):
-            equals = np.allclose(model_weight, target_model_weight)
-            assert equals
-            
-        return model, target_model
-    
-    def rl_models():
-        model = load_model('rl_model.h5', {'fmeasure': fmeasure, 'recall': recall, 'precision': precision})
-    
-        target_model = clone_model(model)
-        target_model.compile(loss='mean_squared_error',  optimizer=SGD(lr=0.0075, momentum=0.5, decay=1e-6, clipnorm=2),
-                      metrics=['accuracy'])
-        target_model.set_weights(model.get_weights())
-        
-        model.summary()
-        target_model.summary()
-    
-        for model_weight, target_model_weight in zip(model.get_weights(), target_model.get_weights()):
-            equals = np.allclose(model_weight, target_model_weight)
-            assert equals
-            
-        return model, target_model
-            
+    eps_decay_steps = 1000000   
 
     env = gym.make('AirHockey-v0')
-    agent = DDQNAgent(models=models(),
-                      n_actions=env.n_actions,
+    env.update(mode=config.mode)
+
+    agent = PGAgent(n_actions=env.n_actions,
+                      initial_model_file='../supervised/keras/models/rgb/robot_model.h5',
                       buffer_size=buffer_size,
                       batch_size=batch_size,
                       discount_rate=discount_rate,
                       eps_min=eps_min,
                       eps_max=eps_max,
                       eps_decay_steps=eps_decay_steps,
-                      processor=gym_air_hockey.DataProcessor())
+                      model_file=robot_model_file)
+
     
-    
-    
-    programmed_action = True
+    reward_buffer = deque([], maxlen=1000)  
     action_distribution = np.zeros((env.n_actions), dtype=np.uint32)
+
     def run_episode():
-        global programmed_action
         
         reward_sum = 0
         state = env.reset()
         done = False
         
         episode_step = 0
-        
         while not done and episode_step < episode_length:
             
             try:
@@ -117,35 +69,35 @@ if __name__ == "__main__":
             except:
                 pass
             
-            action = agent.compute_action(state)
+            action = agent.act(state)
             
             next_state, reward, done, info = env.step(action)
             
             # Store data to experience buffer
-            agent.store_memory((state, action, reward, next_state, done))
+            agent.store_experience((state, action, reward, next_state, done))
             state = np.copy(next_state)
             
             action_distribution[action] += 1
             reward_sum += reward
             
-            # Train
-            if agent.iteration > training_start and agent.iteration % training_interval == 0:
-                if agent.iteration % 100 == 0:
-                    print('Iteration: {}'.format(agent.iteration))
-                    print('Action Distribution: ', end='')
-                    print(action_distribution / sum(action_distribution))
+            # Train DDQNAgent
+            if isinstance(agent, DDQNAgent):
+                if agent.iteration > training_start and (agent.iteration % training_interval == 0):
+                    if agent.iteration % 100 == 0:
+                        print('Iteration: {}'.format(agent.iteration))
+                        print('Action Distribution: ', end='')
+                        print(action_distribution / sum(action_distribution))
+                    agent.train()
                 
-                agent.train_q()
-            
-                plt.clf()
-                plt.plot(agent.loss_buffer)
-                plt.pause(0.00001)
-                
-            # Copy to target
-            if agent.iteration > copy_interval and agent.iteration % copy_interval == 0:
-                agent.update_target_weights()
+                # Copy to target
+                if agent.iteration > copy_interval and (agent.iteration % copy_interval == 0):
+                    agent.update_target_weights()
                 
             episode_step += 1
+
+        # Train PGAgent
+        if isinstance(agent, PGAgent):
+            agent.train()
 
         return reward_sum
     
